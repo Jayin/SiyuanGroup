e945a1f3da2c945403f71101d53923480ae6ef41
Jayin Ton <tonjayin@gmail.com>
2013-12-11 14:20:18 +0800

    client init

---
+++ SiyuanGroup/src/com/alumnigroup/app/acty/Main.java
@@ -0,0 +1,39 @@
+package com.alumnigroup.app.acty;
+
+ 
+import android.os.Bundle;
+import android.view.View;
+
+import com.alumnigroup.app.BaseActivity;
+import com.alumnigroup.app.R;
+/**
+ * 主界面
+ * @author Jayin Ton
+ * @since 2013.12.11;
+ *
+ */
+public class Main extends BaseActivity { 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acty_main);
+	}
+	@Override
+	public void onClick(View v) {
+ 
+	}
+
+	@Override
+	protected void initData() {
+	 
+	}
+
+	@Override
+	protected void initLayout() {
+	 
+
+	}
+
+	 
+
+}
