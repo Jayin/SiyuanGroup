b7487da8ac644c90ac96553046c7124e50dcdf6d
Fritz-Lium <fritzlium@gmail.com>
2013-12-17 23:30:33 +0800

    .sql 文件

--- node/config/sql/activity_status.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: 
--- Date: 2013-12-13 16:55
-*/
-INSERT INTO `activity_status` (`id`,`name`) VALUES (0,'接受报名');
-INSERT INTO `activity_status` (`id`,`name`) VALUES (1,'截止报名');
-INSERT INTO `activity_status` (`id`,`name`) VALUES (2,'活动结束');
-INSERT INTO `activity_status` (`id`,`name`) VALUES (3,'活动取消');
