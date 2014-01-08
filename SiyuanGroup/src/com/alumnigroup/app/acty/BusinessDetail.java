56b892ecc3005fc11ec638285cb54f71ae05790d
Jayin Ton <tonjayin@gmail.com>
2014-01-08 21:55:04 +0800

    add business function

---
+++ SiyuanGroup/src/com/alumnigroup/app/acty/BusinessDetail.java
@@ -0,0 +1,26 @@
+package com.alumnigroup.app.acty;
+
+import android.os.Bundle;
+
+import com.alumnigroup.app.BaseActivity;
+import com.alumnigroup.app.R;
+
+public class BusinessDetail extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acty_businessdetail);
+	}
+
+	@Override
+	protected void initData() {
+
+	}
+
+	@Override
+	protected void initLayout() {
+
+	}
+
+}
