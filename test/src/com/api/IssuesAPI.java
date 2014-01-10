39ad36df9cb71029ae63b128327dd7934c132a97
Jayin Ton <tonjayin@gmail.com>
2014-01-10 22:55:33 +0800

    add test case :issues api

---
+++ test/src/com/api/IssuesAPI.java
@@ -0,0 +1,53 @@
+package com.api;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * 话题API
+ * 
+ * @author Jayin Ton
+ * 
+ */
+public class IssuesAPI {
+	/**
+	 * 获得第page页的话题
+	 * 
+	 * @param page 第page页
+	 * @param responseHandler 处理器
+	 */
+	public void getIssueList(int page, AsyncHttpResponseHandler responseHandler) {
+		if (page <= 0)
+			page = 1;
+		RequestParams params = new RequestParams();
+		params.add("page", page + "");
+		RestClient.get("/api/issues/find", params, responseHandler);
+	}
+    /**
+    * 可以根据'id', 'userid', 'title' 来获取话题列表<br>
+    * 基本上没用，不知道为啥要给这个。不过注意给出page参数
+    * @param params 参数
+    * @param responseHandler 处理器
+    */
+	public void getIssueList(RequestParams params,
+			AsyncHttpResponseHandler responseHandler) {
+		RestClient.get("/api/issues/find", params, responseHandler);
+	}
+	/**
+	 * 搜索
+	 * @param page 第几页
+	 * @param title 标题中包含的关键字
+	 * @param body 话题内容包含关键字
+	 * @param responseHandler 处理器
+	 */
+	public void search(int page,String title,String body,AsyncHttpResponseHandler responseHandler){
+		if (page <= 0)
+			page = 1;
+		RequestParams params = new RequestParams();
+		params.add("page", page + "");
+		if(title!=null && title.trim().length()>0)params.add("title", title);
+		if(body!=null && body.trim().length()>0)params.add("body", body);
+		RestClient.get("/api/issues/search", params, responseHandler);
+	}
+
+}
--- test/src/com/siyuangroup/test/TestMainActivity.java
+++ test/src/com/siyuangroup/test/TestMainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
+import com.api.IssuesAPI;
 import com.api.RestClient;
 import com.api.UserAPI;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -30,95 +31,15 @@ public class TestMainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				//register();
-				// find();
-				//login();
-				getAll();
+                 IssuesAPI api = new IssuesAPI();
+                 api.getIssueList(1, new AsyncHttpResponseHandler(){
+                	 @Override
+                	public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+                		 tv.setText(new String(arg2));
+                	}
+                 });
 			}
 		});
-	}
-	
-	public void getAll() {
-		UserAPI api = new UserAPI();
-		api.getAllMember(1, new AsyncHttpResponseHandler() {
-
-			@Override
-			public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
-		       L.i(new String(arg2));
-		       tv.setText(new String(arg2));
-			}
-		});
-	}
-
-	public void login() {
-		String username = "test1";
-		String password = "123456";
-		RequestParams params = new RequestParams();
-		params.add("username", username);
-		params.add("password", password);
-		RestClient.post("/api/users/login", params,
-				new AsyncHttpResponseHandler() {
-					@Override
-					public void onSuccess(int arg0, Header[] arg1, byte[] data) {
-                              tv.setText(new String(data));
-					}
-
-					@Override
-					public void onFailure(int arg0, Header[] arg1, byte[] data,
-							Throwable arg3) {
-						 tv.setText(new String(data));
-					}
-				});
-	}
-
-	public void find() {
-		RestClient.get("/api/users/find", null, new AsyncHttpResponseHandler() {
-			@Override
-			public void onSuccess(int arg0, Header[] arg1, byte[] data) {
-				tv.setText(new String(data));
-			}
-
-			public void onFailure(int arg0, Header[] arg1, byte[] arg2,
-					Throwable arg3) {
-			};
-		});
-	}
-
-	public void register() {
-
-		String username = "test1";
-		String password = "1234567";
-		String name = "Test name";// profile[name]
-		String nickname = "Test1";
-		String email = "t@mz.com";
-        //profile[grade] 
-		RequestParams params = new RequestParams();
-		params.add("username", username);
-		params.add("password", password);
-		params.add("profile[name]", name);
-		params.add("profile[nickname]", nickname);
-		params.add("profile[email]", email);
-		RestClient.post("/api/users/register", params,
-				new AsyncHttpResponseHandler() {
-
-					@Override
-					public void onFailure(int arg0, Header[] arg1, byte[] arg2,
-							Throwable arg3) {
-						L.i(new String(arg2));
-						// tv.setText(arg0);
-
-					}
-
-					@Override
-					public void onSuccess(int statusCode, Header[] headers,
-							byte[] data) {
-
-						L.i(new String(data));
-						tv.setText(new String(data));
-					}
-
-				});
-
 	}
 
 }
