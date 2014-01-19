20f613426185296c911c8af0fe138e96d4ea3fb4
Jayin Ton <tonjayin@gmail.com>
2014-01-20 02:39:54 +0800

    update editted files

---
+++ SiyuanGroup/src/com/alumnigroup/app/acty/Activities.java
@@ -0,0 +1,30 @@
+package com.alumnigroup.app.acty;
+
+import android.os.Bundle;
+
+import com.alumnigroup.app.BaseActivity;
+import com.alumnigroup.app.R;
+/**
+ * 活动
+ * @author Jayin Ton
+ *
+ */
+public class Activities extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acty_activities);
+	}
+	
+	@Override
+	protected void initData() {
+
+	}
+
+	@Override
+	protected void initLayout() {
+
+	}
+
+}
