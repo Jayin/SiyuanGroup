27b63477c87ea84bf8cf87174470af1226711d07
Jayin Ton <tonjayin@gmail.com>
2014-02-23 19:40:15 +0800

    add new introduction

--- SiyuanGroup/src/com/alumnigroup/app/acty/Introduce.java
+++ SiyuanGroup/src/com/alumnigroup/app/acty/Introduce.java
@@ -1,22 +1,18 @@
 package com.alumnigroup.app.acty;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.text.Html;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+import com.alumnigroup.api.RestClient;
 import com.alumnigroup.app.BaseActivity;
 import com.alumnigroup.app.R;
 
 public class Introduce extends BaseActivity {
+	private WebView webview;
 
-	private ViewPager vgImages4Introduce;
+	// private ViewPager vgImages4Introduce;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,12 +28,20 @@ public class Introduce extends BaseActivity {
 
 	@Override
 	protected void initLayout() {
-//		vgImages4Introduce = (ViewPager) _getView(R.id.acty_introduce_vg_images);
-//		vgImages4Introduce.setAdapter(new ImagePagerAdapter());
-//		vgImages4Introduce.setCurrentItem(0);
-		((TextView)_getView(R.id.tv_introduce)).setText(Html.fromHtml("<p>全站会员<br>在这里你可以查看全部校友信息，看过信息后如果你对该校友感兴趣可以加为好友</p><p>全站动态<br>在这里你可以看到全站最新的动态，你也可以搜索你喜欢的动态</p><p> 校友交流区<br>你可以在这个发送自己的微博，如果你想到一个话题，你可以发起话题邀请校友一起讨论，让校友交流更具有针对性</p><p>我的空间<br>标志性的签名，个性化的标签让你的门面更有特色</p><p>圈子系统<br>寻找你的志同道合朋友</p><p>活动系统<br>增进与统一圈子的朋友感情 </p><p>商务合作<br>校友之间相互帮助，合作共赢</p>"));
-        _getView(R.id.acty_head_btn_back).setOnClickListener(this);
+		webview =(WebView) _getView(R.id.webview_about);
+		WebSettings webSettings = webview.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setSupportZoom(true);
+		webSettings.setAllowFileAccess(true);
+		webSettings.setLoadsImagesAutomatically(true); // 自动加载图片
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setUseWideViewPort(true);
+		webview.setInitialScale(50);
+		webview.loadUrl(RestClient.BASE_URL+"/setting/about.html");
+		
+		_getView(R.id.acty_head_btn_back).setOnClickListener(this);
 	}
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -49,64 +53,4 @@ public class Introduce extends BaseActivity {
 			break;
 		}
 	}
-
-	
-	/**
-	 * 功能介绍的图片适配器，是固定是形式
-	 * @author vector
-	 *
-	 */
-	private class ImagePagerAdapter extends PagerAdapter {
-
-		private int[] images = {R.drawable.a_lyf_1,R.drawable.a_lyf_1,R.drawable.a_lyf_1,R.drawable.a_lyf_1};
-		private LayoutInflater inflater = inflater = getLayoutInflater();;
-		
-		@Override
-		public void destroyItem(ViewGroup container, int position, Object object) {
-			container.removeView((View) object);
-		}
-
-		@Override
-		public int getCount() {
-			return images.length;
-		}
-
-		/**
-		 * 这里填充数据
-		 */
-		public Object instantiateItem(ViewGroup view, int position) {
-			View imageLayout = inflater.inflate(
-					R.layout.item_acty_introduce_image, view, false);
-			/**
-			 * 设置图片
-			 */
-			ImageView  image = (ImageView) imageLayout.findViewById(R.id.item_acty_introduce_image);
-			image.setImageDrawable(getResources().getDrawable(images[position]));
-			
-			/**
-			 * 如果是最后一张图片就多一个按钮
-			 */
-			if(position == images.length-1){
-				View btnBack = imageLayout.findViewById(R.id.item_acty_introduce_btn_back);
-				btnBack.setVisibility(View.VISIBLE);
-				btnBack.setOnClickListener(new OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						finish();
-					}
-				});
-			}
-			
-			view.addView(imageLayout, 0);
-			return imageLayout;
-		}
-
-		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return arg0 == arg1;
-		}
-
-	}
-
 }
